ff1cdccd007c3acafbf567a0f070d3d1edcb45ff
Joel Dunham <jdunham@droneaviationcorp.com>
2017-02-27 15:22:56 -0500

    Updated to make use of the messaging definitions

--- EEPROM_XPORT_CONSOLE_Python3.py
+++ EEPROM_XPORT_CONSOLE_Python3.py
@@ -3,12 +3,11 @@
 
 import serial # may decide to switch to Joels serial connection files in dacutils 1.0 conection
 import sys
-from dacutils import messagedefinition  #from dacutils import 
-from dacutils import messages_V3
+from dacutils.messagedefinition import MessageDefinition, HeaderDefinition
+from dacutils import messages
+import messages_V3
 from dacutils import checksum
-#import ctypes
 
-# pyserial
 from struct import *
 comportnum = input('enter comport number: ')
 comport = 'com'+comportnum
@@ -16,53 +15,26 @@ ser = serial.Serial(comport)
 baudrate = input('enter baud rate: ')
 ser.baudrate = baudrate
 
-class EEPROM_UPDATES(object):
-    def __init__(self, sync1, sync2, sync3, spare, messageID, messageSize, hcsum, csum,
-                 action, value1, value2, value3, spare1, spare2, align, align2):
-        self.sync1 = sync1
-        self.sync2 = sync2
-        self.sync3 = sync3
-        self.spare = spare
-        self.messageID = messageID
-        self.messageSize = messageSize
-        self.hcsum = hcsum
-        self.csum = csum
-        self.action = action
-        self.value1 = value1
-        self.value1 = value1
-        self.value1 = value1
-        self.spare1 = spare1
-        self.spare2 = spare2
-        self.align = align
-        self.align2 = align2
-
 
 def main():
+    # Define the message to send
+    header = HeaderDefinition(messages_V3.get_header_format(), checksum.compute_check_sum)
+    eeprom_updates_message = MessageDefinition("eeprom_updates", "Message to update the EEPROM",
+                                               0, messages_V3.get_eeprom_updates_format(), header,
+                                               False, messages_V3.MESSAGE_IDS["EEPROM_UPDATES"])
 
-    #EEPROM_UPDATES = 
+    # Set all values
+    eeprom_updates_message.mapped["action"].values = 0x1
+    eeprom_updates_message.mapped["value1"].values =\
+        int(input("Please input value1, in hex w/o 0x prefix: "), 32)
+    print(eeprom_updates_message.mapped["value1"].values)
+    eeprom_updates_message.mapped["value2"].values = int(input("Please input value2, in hex w/o 0x prefix: "), 32)
+    print(eeprom_updates_message.mapped["value2"].values)
+    eeprom_updates_message.mapped["value3"].values = int(input("Please input value3, in hex w/o 0x prefix: "), 32)
+    print(eeprom_updates_message.mapped["value3"].values)
 
-    #for sending messages
-    header = messagedefinition.HeaderDefinition(messages_V3.get_header_format(), checksum.compute_check_sum) #EEPROM_UPDATES.
-    
-    #message = EEPROM_UPDATES
-    MESSAGE_ID_EEPROM_VARIBLES = 500
-    messageID = MESSAGE_ID_EEPROM_VARIBLES #message.messageID = MESSAGE_ID_EEPROM_VARIBLES
-    EEPROM_UPDATES.sync1 = 0xa3 #message.DATALINK_SYNC0 = 0xa3
-    EEPROM_UPDATES.sync2 = 0xb2 #message.DATALINK_SYNC1 = 0xb2
-    EEPROM_UPDATES.sync3 = 0xc1 #message.DATALINK_SYNC2 = 0xc1
-    EEPROM_UPDATES.action = 0x1
-    EEPROM_UPDATES.value1 = int(input("Please input value1, in hex w/o 0x prefix: "), 32) #add ),16) if not using 0x before hex number #was message.value1
-    print (EEPROM_UPDATES.value1)
-    EEPROM_UPDATES.value2 = int(input("Please input value2, in hex w/o 0x prefix: "), 32)           #was message.value2
-    print (EEPROM_UPDATES.value2)
-    EEPROM_UPDATES.value3 = int(input("Please input value3, in hex w/o 0x prefix: "), 32)   # was message.value3
-    print (EEPROM_UPDATES.value3)
-
-    EEPROM_UPDATES_message = messagedefinition.MessageDefinition("EEPROM_UPDATES", "EEPROM_UPDATES_MESSAGE", messageID,
-                                                          messages_V3.get_EEPROM_UPDATES_format(), header, False )
-
-    #checkSumEncode( message, sys.getsizeof(EEPROM_UPDATES) )
-    encoded_message = EEPROM_UPDATES.encode()
+    # Encode and send
+    encoded_message = eeprom_updates_message.encode()
     ser.write(encoded_message)
     
     s = 'message sent'
--- messages_V3.py
+++ messages_V3.py
@@ -1,37 +1,16 @@
 from dacutils.messagedefinition import FieldDefinition
 
-MESSAGE_ID_NAME = "messageID"
 MESSAGE_IDS = {
-    "status": 500,  # GPIU to GCS
+    "EEPROM_UPDATES": 1000  # Set to correct value later
 }
 
 
-def get_header_format():
+def get_eeprom_updates_format():
     """
-    Creates and returns a new header format - used for each message definition to maintain memory independence
-     between the message headers
-    """
-    header_format = [
-        # An ordered list of the fields in the header
-        # The header precedes every subsequent message defined here
-        FieldDefinition("sync1", "Sync byte 1", "uchar", 1, 0xa3),
-        FieldDefinition("sync2", "Sync byte 2", "uchar", 1, 0xb2),
-        FieldDefinition("sync3", "Sync byte 3", "uchar", 1, 0xc1),
-        FieldDefinition("spare", "spare, occasionally used as a unique index for single sends", "uchar", 1, 0),
-        FieldDefinition("messageID", "Message identifier", "int", 1, 0),
-        FieldDefinition("messageSize", "Message size, including the header", "int", 1, 0),
-        FieldDefinition("hcsum", "Header checksum", "uint", 1, 0),
-        FieldDefinition("csum", "checksum", "uint", 1, 0)
-    ]
-    return header_format
-
-
-def get_EEPROM_UPDATES_format():
-    """
-    Creates and returns a new status format - maintain memory independence of the format
+    Creates and returns a new eeprom update format - maintain memory independence of the format
     """
 
-    status_format = [
+    update_format = [
         FieldDefinition("action", "desired action for peripheral PCB, write to EEPROM", "uchar", 1, 0),
         FieldDefinition("value1", "value for the 1st 4 bytes/ 1st & 2nd EEPROM variables", "uchar", 1, 0),
         FieldDefinition("value2", "value for the 2nd 4 bytes/ 3rd & 4th EEPROM variables", "uchar", 1, 0),
@@ -41,4 +20,4 @@ def get_EEPROM_UPDATES_format():
         FieldDefinition("align", "byte alignment", "uchar", 1, 0),
         FieldDefinition("align2", "byte alignment", "uchar", 1, 0)
     ]
-    return status_format
+    return update_format
