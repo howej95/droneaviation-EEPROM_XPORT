7554a9c0d2e910918ef10d0706481913f4e2c77e
Jakob Howe <howej95@users.noreply.github.com>
2017-03-03 15:33:26 -0500

    Add files via upload

--- messages_V3.py
+++ messages_V3.py
@@ -12,11 +12,10 @@ def get_eeprom_updates_format():
 
     update_format = [
         FieldDefinition("action", "desired action for peripheral PCB, write to EEPROM", "uchar", 1, 0),
-        FieldDefinition("value1", "value for the 1st 4 bytes/ 1st & 2nd EEPROM variables", "uchar", 1, 0),
-        FieldDefinition("value2", "value for the 2nd 4 bytes/ 3rd & 4th EEPROM variables", "uchar", 1, 0),
-        FieldDefinition("value2", "value for the 3rd 4 bytes/ 5th & 6th EEPROM variables", "uchar", 1, 0),
-        FieldDefinition("spare", "spare, occasionally used as a unique index for single sends", "uchar", 1, 0),
-        FieldDefinition("spare", "spare, occasionally used as a unique index for single sends", "uchar", 1, 0),
+        FieldDefinition("value1", "value for the 1st 4 bytes/ 1st & 2nd EEPROM variables", "ulong", 1, 32),
+        FieldDefinition("value2", "value for the 2nd 4 bytes/ 3rd & 4th EEPROM variables", "ulong", 1, 32),
+        FieldDefinition("value3", "value for the 3rd 4 bytes/ 5th & 6th EEPROM variables", "ulong", 1, 32),
+        FieldDefinition("spare1", "spare, occasionally used as a unique index for single sends", "uchar", 1, 0),
         FieldDefinition("align", "byte alignment", "uchar", 1, 0),
         FieldDefinition("align2", "byte alignment", "uchar", 1, 0)
     ]
